3ffb8c456ba2e8755de4aefcdc529b596c355bc9
mic_anghileri <micheleanghileri@gmail.com>
2020-07-23 14:09:14 +0200

    testo es js

---
+++ main.js
@@ -0,0 +1,14 @@
+// Descrizione:
+// Creare uno slider di immagini
+// Potete usare le immagini che desiderate.
+// Per facilitarvi la vita usate immagini delle stesse dimensioni :)
+// Lo slider prevederà due frecce (icone) che permettono di scorrere tra le immagini dello slider
+// Utilizziamo una classe first e last  per capire quali sono la prima e ultima immagine dello slider
+// Utilizziamo una classe active per aiutarci a capire qual è l’immagine attuale da visualizzare
+// nello slider
+// Bonus:
+// Clicchiamo sui pallini e mostriamo l’immagine corrispondente
+// Generiamo i pallini con JS
+// Consiglio del giorno:
+// Come sempre è importante usare la documentazione di jQuery
+// per scoprire/rinfrescare memoria sulle funzioni che potrebbero esserci utili.
