97a06bc4472a83c943c5f3ddb0f1d64229bc0a4f
mic_anghileri <micheleanghileri@gmail.com>
2020-07-23 16:48:29 +0200

    definitivo js

--- main.js
+++ main.js
@@ -12,3 +12,52 @@
 // Consiglio del giorno:
 // Come sempre è importante usare la documentazione di jQuery
 // per scoprire/rinfrescare memoria sulle funzioni che potrebbero esserci utili.
+
+$(document).ready(function(){
+
+    $('.next').click(function(){
+        nextPic();
+    });
+
+    $('.prev').click(function(){
+        prevPic();
+    });
+
+// *****funzioni*****
+
+// funzione per andare all'immagine successiva
+    function nextPic(){
+        // fisso la classe active con due var
+        var activePic = $('.images img.active');
+        var activeCircle = $('.nav i.active');
+        // rimuovo la classe active
+        activePic.removeClass('active');
+        activeCircle.removeClass('active');
+        // aggiungo la classe active agli elementi successivi
+        if (activePic.hasClass('last')) {
+            $('.images img.first').addClass('active');
+            $('.nav i.first').addClass('active');
+        } else {
+            activePic.next().addClass('active');
+            activeCircle.next().addClass('active');
+        }
+    };
+
+// funzione per tornare all'immagine precedente
+    function prevPic() {
+        // fisso la classe active con due var
+        var activePic = $('.images img.active');
+        var activeCircle = $('.nav i.active');
+        // rimuovo la classe active
+        activePic.removeClass('active');
+        activeCircle.removeClass('active');
+        // aggiungo la classe active agli elementi successivi
+        if (activePic.hasClass('first')) {
+            $('.images img.last').addClass('active');
+            $('.nav i.last').addClass('active');
+        } else {
+            activePic.prev().addClass('active');
+            activeCircle.prev().addClass('active');
+        }
+    };
+});
